43ecb993eee97decaa876c262cbc56ab79d368bc
Pedro Jr Solatorio <solatoriop@gmail.com>
2025-06-17 14:56:57 +0800

    Restore some lines in type.ts that are removed but are actually needed

--- client/src/navigation/types.ts
+++ client/src/navigation/types.ts
@@ -1,3 +1,11 @@
 export type RootStackParamList = {
   Login: undefined;
+  RegisterSelection: undefined;
+  RegisterDriver: undefined;
+  RegisterPassenger: undefined;
+  // AdminHome: undefined; to be checked if i really need this
+  // DriverHome: undefined;
+  // PassengerHome: undefined;
+  // BookRide: undefined;
+  // Add more screens here as needed
 };
